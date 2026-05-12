ae3739ba57c6352d580034298b4a8b9caf9b0493
Kuchen <nestorkotov71@gmail.com>
2026-05-12 12:35:35 +0300

    132

---
+++ ksc_connector.py
@@ -0,0 +1,17 @@
+class ksc_connector:
+    """
+    Attributes:
+        host:
+            winhostName
+            domain_name domainName
+            os_name
+            os_version
+            KSC_hostName
+                        HWInvStorageSrvViewName
+            mac,hardware,serial_number,InvNum
+            status
+            description
+            
+    
+    
+    """
